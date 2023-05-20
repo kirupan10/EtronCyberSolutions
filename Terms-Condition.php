45d2432de0531ac64da37c84242945afb7a9116b
kirupan10 <ikirupan@gmail.com>
2023-05-20 16:11:06 +0530

    20.05.2023.2.16.11

--- Terms-Condition.php
+++ Terms-Condition.php
@@ -79,79 +79,8 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <footer id="footer">
-
-    <div class="footer-newsletter">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-            <h4>Join Our Newsletter</h4>
-            <p>Experience security and peace of mind with our comprehensive security services.</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Etron Cyber Solutions</h3>
-            <p>
-               <br>
-              Scarborough Ontario <br><br>
-              <strong>Phone:</strong> 416 418 3926<br>
-              <strong>Email:</strong> etroncybersolution@gmail.com<br>
-            </p>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="/">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="/">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="/">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="Terms-Condition">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="Privacy-Policy">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="Network-security">Network security</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="Website-security">Website security</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="Security-auditing">Security auditing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="Employee-awareness">Employee Awareness Training program</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="Email-social-engineering">Email social engineering</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Social Networks</h4>
-            <p>Connect with us on social media!</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container footer-bottom clearfix">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Etron Cyber Solutions</span></strong>. All Rights Reserved
-      </div>
-    </div>
-  </footer><!-- End Footer -->
+<?php include 'config/footer.php';?>
+  <!-- ======= Footer ======= -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
       class="bi bi-arrow-up-short"></i></a>
